d83ce8e34b9f80037ab8b66a56323467a5273ddf
JuanEstradaV <juanjo.jjev@gmail.com>
2023-11-12 15:34:20 -0500

    Angular Home y About us page entrega 1

--- veterinariaFrontend/src/app/pages/home/home.component.ts
+++ veterinariaFrontend/src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CarruselComponent } from '../../components/carrusel/carrusel.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CarruselComponent, NavbarComponent, RouterOutlet],
+  imports: [CommonModule, CarruselComponent, NavbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
