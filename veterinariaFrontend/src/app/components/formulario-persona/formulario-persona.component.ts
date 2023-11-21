42ce35b99765564005c83f0c2f9f3ec6e5bde274
Adams_GMR <juan_adams82212@elpoli.edu.co>
2023-11-20 23:22:18 -0500

    formulariopersonas

--- veterinariaFrontend/src/app/components/formulario-persona/formulario-persona.component.ts
+++ veterinariaFrontend/src/app/components/formulario-persona/formulario-persona.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFloppyDisk, faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-formulario-persona',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './formulario-persona.component.html',
   styleUrl: './formulario-persona.component.css'
 })
 export class FormularioPersonaComponent {
-
+  guardar = faFloppyDisk
+  cancelar = faArrowLeft
 }
