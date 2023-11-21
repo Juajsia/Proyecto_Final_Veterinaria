dd52e629bdf7b9700238c38bbdaba5c111ca3e4f
JuanEstradaV <juanjo.jjev@gmail.com>
2023-11-20 23:18:37 -0500

    Página Persona

--- veterinariaFrontend/src/app/app.routes.ts
+++ veterinariaFrontend/src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MascotaComponent } from './pages/mascota/mascota.component';
-import { loginGuard, rolVeterinario } from './guards/login.guard';
+import { loginGuard, rolAdmin, rolVeterinario } from './guards/login.guard';
 import { FormularioMascotaComponent } from './components/formulario-mascota/formulario-mascota.component';
+import { PersonaComponent } from './pages/persona/persona.component';
 
 export const routes: Routes = [{
     title: 'Home',
@@ -28,4 +29,9 @@ export const routes: Routes = [{
     path: 'mascota/formulario',
     component: FormularioMascotaComponent,
     canActivate: [loginGuard, rolVeterinario]
+},{
+    title: 'Persona',
+    path: 'persona',
+    component: PersonaComponent,
+    canActivate: [loginGuard, rolAdmin]
 }];
