0b3a698b914da653dc8d2d69cb428ce5da73c43b
JuanEstradaV <juanjo.jjev@gmail.com>
2023-12-02 02:02:49 -0500

    cosas

--- veterinariaFrontend/src/app/components/navbar/navbar.component.ts
+++ veterinariaFrontend/src/app/components/navbar/navbar.component.ts
@@ -2,15 +2,18 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { Guard, loginGuard } from '../../guards/login.guard';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink,FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  barra = faBars
   loginStatus = Guard()
   router = inject(Router)
   rol = localStorage.getItem('rol')
